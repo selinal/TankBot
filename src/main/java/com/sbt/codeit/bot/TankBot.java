ee7fa001e14f03fad8d9ba06c3968b205b643536
sbt-galimov-rr <RRGalimov.SBT@sberbank.ru>
2017-04-18 15:16:44 +0400

    чистка

--- src/main/java/com/sbt/codeit/bot/TankBot.java
+++ src/main/java/com/sbt/codeit/bot/TankBot.java
@@ -3,31 +3,36 @@ package com.sbt.codeit.bot;
 import com.sbt.codeit.core.control.GameController;
 import com.sbt.codeit.core.control.ServerListener;
 
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
-import java.rmi.server.UnicastRemoteObject;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
 
-public class TankBot  {
+/**
+ * Created by sbt-selin-an on 18.04.2017.
+ */
+public class TankBot implements ServerListener {
 
-    private static GameController server;
-    private static ServerListener client;
+    private GameController server;
 
-    public static void main(String... args) {
+    public TankBot(GameController server) {
+        this.server = server;
+    }
+
+    public void update(ArrayList<ArrayList<Character>> arrayList) throws RemoteException {
         try {
-            Registry registry = LocateRegistry.getRegistry(ServerListener.HOST, ServerListener.PORT);
-            server = (GameController) registry.lookup(ServerListener.STUB_NAME);
-
-            ServerListener tankBot = new ServerListenerImpl(server);
-
-            client = (ServerListener) UnicastRemoteObject.exportObject(tankBot, 0);
-            server.register(client, ((ServerListenerImpl)tankBot).getName());
-            server.start(client);
-            synchronized (tankBot) {
-                tankBot.wait();
-            }
+            // place your code here
+            // analise field
+            // and perform action
+             server.left(this);
+             server.right(this);
+             server.fire(this);
         } catch (Exception e) {
             e.printStackTrace();
             System.exit(1);
         }
+    }
+
+    public String getName() {
+//        throw new RuntimeException("Place your command name here");
+        return "Rx";
     }
 }
--- src/main/java/com/sbt/codeit/core/control/ServerListener.java
+++ src/main/java/com/sbt/codeit/core/control/ServerListener.java
@@ -4,14 +4,7 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 
-/**
- * Created by sbt-galimov-rr on 09.02.2017.
- */
 public interface ServerListener extends Remote {
-
-    public String HOST = "localhost";
-    public int PORT = 2017;
-    public String STUB_NAME = "GameController";
 
     void update(ArrayList<ArrayList<Character>> field) throws RemoteException;
 
