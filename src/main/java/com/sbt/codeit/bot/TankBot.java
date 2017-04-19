307e957c280d83ad62ba1fa88a168cae0fd4afe6
sbt-galimov-rr <RRGalimov.SBT@sberbank.ru>
2017-04-19 15:49:34 +0400

    random bot

--- src/main/java/com/sbt/codeit/bot/TankBot.java
+++ src/main/java/com/sbt/codeit/bot/TankBot.java
@@ -5,30 +5,44 @@ import com.sbt.codeit.core.control.ServerListener;
 
 import java.rmi.RemoteException;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class TankBot implements ServerListener {
 
     private GameController server;
+
+    private Random random = new Random();
+    private int i = 0;
 
     public TankBot(GameController server) {
         this.server = server;
     }
 
     public void update(ArrayList<ArrayList<Character>> arrayList) throws RemoteException {
-        try {
-            // place your code here
-            // analise field
-            // and perform action
-            server.right(this);
-            server.start(this);
-            server.fire(this);
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.exit(1);
+        server.start(this);
+        server.fire(this);
+        i++;
+        if(i % 5 != 0) {
+            return;
+        }
+        switch (random.nextInt(4)){
+            case 0:
+                server.left(this);
+                break;
+            case 1:
+                server.right(this);
+                break;
+            case 2:
+                server.up(this);
+                break;
+            case 3:
+                server.down(this);
+                break;
         }
     }
 
     public String getName() {
-        throw new RuntimeException("Place your command name here");
+        return "Team1";
     }
+
 }
